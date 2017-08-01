1a66bba025f076f980840f084b6a79e080d572a7
Brandon Williams <mbw234@gmail.com>
2017-07-31 21:26:54 -0400

    simplify

--- Sources/SnapshotTesting/SceneKit.swift
+++ Sources/SnapshotTesting/SceneKit.swift
@@ -13,9 +13,7 @@ import SceneKit
 #if os(iOS)
   extension SCNScene: Snapshot {
     public var snapshotFormat: UIImage {
-      let vc = UIViewController()
       let sceneView = SCNView(frame: .init(x: 0, y: 0, width: 500, height: 500))
-      vc.view = sceneView
       sceneView.scene = self
 
       let snapshot = sceneView.snapshot()
