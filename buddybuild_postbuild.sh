d0d13d87c610a7bdd05196e7f2ee22a293da8ee4
Brandon Williams <mbw234@gmail.com>
2017-11-15 21:09:08 -0500

    wip

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -2,4 +2,3 @@
 
 xcodebuild test -scheme SnapshotTesting-Package -destination platform="macOS"
 xcodebuild test -scheme SnapshotTesting-Package -destination platform="iOS Simulator,name=iPhone 8,OS=11.2"
-# swiftlint
