5c730c6c7a2735e72077722a443f0d1bdd48f587
Matt Diephouse <matt@diephouse.com>
2018-02-05 09:08:02 -0500

    Create an xcodeproj with macOS and iOS frameworks

---
+++ Sources/SnapshotTesting.h
@@ -0,0 +1,10 @@
+@import Foundation;
+
+//! Project version number for SnapshotTesting.
+FOUNDATION_EXPORT double SnapshotTestingVersionNumber;
+
+//! Project version string for SnapshotTesting.
+FOUNDATION_EXPORT const unsigned char SnapshotTestingVersionString[];
+
+@import CoreGraphics;
+#import <WebKit/WKSnapshotConfiguration.h>
