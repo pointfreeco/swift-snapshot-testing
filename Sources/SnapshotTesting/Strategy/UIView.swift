ee5c4b2d109591f6d1f62671523c67b5e1a7b4c9
Stephen Celis <stephen@stephencelis.com>
2018-10-11 22:31:19 -0400

    Update UIView.swift

--- Sources/SnapshotTesting/Strategy/UIView.swift
+++ Sources/SnapshotTesting/Strategy/UIView.swift
@@ -8,7 +8,7 @@ extension Strategy {
       precondition(!($0 is WKWebView), """
 WKWebView must be snapshot using the "webView" strategy.
 
-    assertSnapshot(view, with: .webView)
+    assertSnapshot(matching: view, with: .webView)
 """)
 
       return $0.layer
