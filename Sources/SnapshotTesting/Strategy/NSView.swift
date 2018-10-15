56c2101a41080632a0b6cc5cf1fe9f1ded103723
Stephen Celis <stephen@stephencelis.com>
2018-10-15 19:37:30 -0400

    Revert nsview snapping

--- Sources/SnapshotTesting/Strategy/NSView.swift
+++ Sources/SnapshotTesting/Strategy/NSView.swift
@@ -11,9 +11,7 @@ WKWebView must be snapshot using the "webView" strategy.
     assertSnapshot(matching: view, with: .webView)
 """)
 
-      let imageRepresentation = $0.bitmapImageRepForCachingDisplay(in: $0.bounds)!
-      $0.cacheDisplay(in: $0.bounds, to: imageRepresentation)
-      return NSImage(cgImage: imageRepresentation.cgImage!, size: $0.bounds.size)
+      return NSImage(data: $0.dataWithPDF(inside: $0.bounds))!
     }
   }
 }
