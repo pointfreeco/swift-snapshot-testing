89750e145cca401f3db71a9e49409884e51323fb
Stephen Celis <stephen@stephencelis.com>
2018-11-03 11:49:32 -0400

    Feed precision through

--- Sources/SnapshotTesting/Strategy/SpriteKit.swift
+++ Sources/SnapshotTesting/Strategy/SpriteKit.swift
@@ -8,21 +8,21 @@ import UIKit
 
 #if os(macOS)
 extension Strategy where A == SKScene, B == NSImage {
-  public static func scene(size: CGSize) -> Strategy {
-    return .skScene(size: size)
+  public static func scene(size: CGSize, precision: Float = 1) -> Strategy {
+    return .skScene(size: size, precision: precision)
   }
 }
 #elseif os(iOS) || os(tvOS) || os(watchOS)
 extension Strategy where A == SKScene, B == UIImage {
-  public static func scene(size: CGSize) -> Strategy {
-    return .skScene(size: size)
+  public static func scene(size: CGSize, precision: Float = 1) -> Strategy {
+    return .skScene(size: size, precision: precision)
   }
 }
 #endif
 
 fileprivate extension Strategy where A == SKScene, B == Image {
-  static func skScene(size: CGSize) -> Strategy {
-    return Strategy<View, Image>.view.pullback { scene in
+  static func skScene(size: CGSize, precision: Float) -> Strategy {
+    return Strategy<View, Image>.view(precision: precision).pullback { scene in
       let view = SKView(frame: .init(x: 0, y: 0, width: size.width, height: size.height))
       view.presentScene(scene)
       return view
