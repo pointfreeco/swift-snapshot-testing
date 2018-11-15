c3df3f4061784a2046047bf8b5618a51cbd47dfa
Stephen Celis <stephen.celis@gmail.com>
2018-11-15 16:49:42 -0500

    UIView/Controller Strategy Improvements (#92)
    * UIView/Controller Strategy Improvements
    
    * Update View.swift
    
    * Add traits to views

--- Sources/SnapshotTesting/AssertSnapshot.swift
+++ Sources/SnapshotTesting/AssertSnapshot.swift
@@ -40,13 +40,12 @@ public func assertSnapshot<A, B>(
   let recording = recording || record
 
   do {
-    let snapshotDirectoryUrl: URL = {
-      let fileUrl = URL(fileURLWithPath: "\(file)")
-      let directoryUrl = fileUrl.deletingLastPathComponent()
-      return directoryUrl
-        .appendingPathComponent("__Snapshots__")
-        .appendingPathComponent(fileUrl.deletingPathExtension().lastPathComponent)
-    }()
+    let fileUrl = URL(fileURLWithPath: "\(file)")
+    let fileName = fileUrl.deletingPathExtension().lastPathComponent
+    let directoryUrl = fileUrl.deletingLastPathComponent()
+    let snapshotDirectoryUrl: URL = directoryUrl
+      .appendingPathComponent("__Snapshots__")
+      .appendingPathComponent(fileName)
 
     let identifier: String
     if let name = name {
@@ -103,8 +102,9 @@ public func assertSnapshot<A, B>(
     let artifactsUrl = URL(
       fileURLWithPath: ProcessInfo.processInfo.environment["SNAPSHOT_ARTIFACTS"] ?? NSTemporaryDirectory()
     )
-    try fileManager.createDirectory(at: artifactsUrl, withIntermediateDirectories: true)
-    let failedSnapshotFileUrl = artifactsUrl.appendingPathComponent(snapshotFileUrl.lastPathComponent)
+    let artifactsSubUrl = artifactsUrl.appendingPathComponent(fileName)
+    try fileManager.createDirectory(at: artifactsSubUrl, withIntermediateDirectories: true)
+    let failedSnapshotFileUrl = artifactsSubUrl.appendingPathComponent(snapshotFileUrl.lastPathComponent)
     try strategy.diffable.to(diffable).write(to: failedSnapshotFileUrl)
 
     if !attachments.isEmpty {
