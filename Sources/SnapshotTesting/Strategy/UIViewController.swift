307370d3fe1a0d3d1554575d9e7bdc7458315de9
Stephen Celis <stephen@stephencelis.com>
2018-11-28 10:23:16 -0500

    Add back overload

--- Sources/SnapshotTesting/Strategy/UIViewController.swift
+++ Sources/SnapshotTesting/Strategy/UIViewController.swift
@@ -7,7 +7,7 @@ extension Strategy where Snapshottable == UIViewController, Format == UIImage {
   }
 
   public static func image(
-    on config: ViewImageConfig = .init(),
+    on config: ViewImageConfig,
     precision: Float = 1,
     traits: UITraitCollection = .init()
     )
@@ -17,6 +17,23 @@ extension Strategy where Snapshottable == UIViewController, Format == UIImage {
         snapshotView(
           config: config,
           traits: traits,
+          view: viewController.view,
+          viewController: viewController
+        )
+      }
+  }
+
+  public static func image(
+    precision: Float = 1,
+    size: CGSize? = nil,
+    traits: UITraitCollection = .init()
+    )
+    -> Strategy {
+
+      return SimpleStrategy.image(precision: precision).asyncPullback { viewController in
+        snapshotView(
+          config: .init(safeArea: .zero, size: size, traits: traits),
+          traits: .init(),
           view: viewController.view,
           viewController: viewController
         )
