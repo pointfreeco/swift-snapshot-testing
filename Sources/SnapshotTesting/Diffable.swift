f5a778e525fbbe89b69a95e39e1926e60a67dcd4
Stephen Celis <stephen.celis@gmail.com>
2018-11-30 10:25:49 -0500

    Update Diffable.swift

--- Sources/SnapshotTesting/Diffable.swift
+++ Sources/SnapshotTesting/Diffable.swift
@@ -5,7 +5,7 @@ public struct Diffable<Value> {
   /// Converts a value _to_ data.
   public let to: (Value) -> Data
 
-  /// Produces a value _ from_ data.
+  /// Produces a value _from_ data.
   public let fro: (Data) -> Value
 
   /// Compares two values. If the values do not match, returns a failure message and artifacts describing the failure.
