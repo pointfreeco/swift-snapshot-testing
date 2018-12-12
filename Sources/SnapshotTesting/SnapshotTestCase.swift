44a2bdb6ede77b03accdcd8170ff0b453ec77881
Stephen Celis <stephen.celis@gmail.com>
2018-12-12 13:36:59 -0500

    Update SnapshotTestCase.swift

--- Sources/SnapshotTesting/SnapshotTestCase.swift
+++ Sources/SnapshotTesting/SnapshotTestCase.swift
@@ -107,7 +107,7 @@ open class SnapshotTestCase: XCTestCase {
         #if !os(Linux)
         XCTContext.runActivity(named: "Attached Failure Diff") { activity in
           attachments.forEach {
-            activity.add($0.rawValue)
+            activity.add($0)
           }
         }
         #endif
