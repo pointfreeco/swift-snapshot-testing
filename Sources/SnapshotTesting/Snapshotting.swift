44cc918e212eb917f94fe50108c707501ebf41b2
Stephen Celis <stephen.celis@gmail.com>
2018-12-21 10:52:10 -0500

    Update Sources/SnapshotTesting/Snapshotting.swift
    Co-Authored-By: mbrandonw <mbw234@gmail.com>

--- Sources/SnapshotTesting/Snapshotting.swift
+++ Sources/SnapshotTesting/Snapshotting.swift
@@ -64,7 +64,7 @@ public struct Snapshotting<Value, Format> {
   /// }
   /// ```
   ///
-  /// Here we took the strategy that snapshots `UIView`'s as `UIImage`'s and pulled it back to work on
+  /// Here we took the strategy that snapshots `UIView`s as `UIImage`s and pulled it back to work on
   /// `UIViewController`'s by using the function `(UIViewController) -> UIView` that simply plucks the
   /// view out of the controller.
   ///
