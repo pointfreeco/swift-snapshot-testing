5cdeca09de2115fe924c25e32b10871c75437e38
Stephen Celis <stephen.celis@gmail.com>
2018-12-21 21:58:01 -0500

    Update URLRequest.swift

--- Sources/SnapshotTesting/Snapshotting/URLRequest.swift
+++ Sources/SnapshotTesting/Snapshotting/URLRequest.swift
@@ -18,7 +18,7 @@ extension Snapshotting where Value == URLRequest, Format == String {
         .map { ["\n\(String(decoding: $0, as: UTF8.self))"] }
         ?? []
       } else {
-        throw NSError.init(domain: "co.pointfree.Never", code: 1, userInfo: nil)
+        throw NSError(domain: "co.pointfree.Never", code: 1, userInfo: nil)
       }
     }
     catch {
