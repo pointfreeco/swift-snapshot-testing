277896f20e377950159c63f987d6440474378a5b
Brandon Williams <mbw234@gmail.com>
2019-03-29 16:27:17 -0400

    backward compatible api.

--- Sources/SnapshotTesting/Snapshotting/URLRequest.swift
+++ Sources/SnapshotTesting/Snapshotting/URLRequest.swift
@@ -2,31 +2,38 @@ import Foundation
 
 extension Snapshotting where Value == URLRequest, Format == String {
   /// A snapshot strategy for comparing requests based on raw equality.
-  public static let raw = SimplySnapshotting.lines.pullback { (request: URLRequest) in
-    let method = "\(request.httpMethod ?? "GET") \(request.url?.absoluteString ?? "(null)")"
+  public static let raw = Snapshotting.raw(pretty: false)
 
-    let headers = (request.allHTTPHeaderFields ?? [:])
-      .map { key, value in "\(key): \(value)" }
-      .sorted()
+  /// A snapshot strategy for comparing requests based on raw equality.
+  ///
+  /// - Parameter pretty: Attempts to pretty print the body of the request (supports JSON).
+  public static func raw(pretty: Bool) -> Snapshotting {
+    return SimplySnapshotting.lines.pullback { (request: URLRequest) in
+      let method = "\(request.httpMethod ?? "GET") \(request.url?.absoluteString ?? "(null)")"
 
-    let body: [String]
-    do {
-      if #available(iOS 11.0, *) {
-      body = try request.httpBody
-        .map { try JSONSerialization.jsonObject(with: $0, options: []) }
-        .map { try JSONSerialization.data(withJSONObject: $0, options: [.prettyPrinted, .sortedKeys]) }
-        .map { ["\n\(String(decoding: $0, as: UTF8.self))"] }
-        ?? []
-      } else {
-        throw NSError.init(domain: "", code: 1, userInfo: nil)
+      let headers = (request.allHTTPHeaderFields ?? [:])
+        .map { key, value in "\(key): \(value)" }
+        .sorted()
+
+      let body: [String]
+      do {
+        if pretty, #available(iOS 11.0, macOS 10.13, tvOS 11.0, *) {
+          body = try request.httpBody
+            .map { try JSONSerialization.jsonObject(with: $0, options: []) }
+            .map { try JSONSerialization.data(withJSONObject: $0, options: [.prettyPrinted, .sortedKeys]) }
+            .map { ["\n\(String(decoding: $0, as: UTF8.self))"] }
+            ?? []
+        } else {
+          throw NSError.init(domain: "", code: 1, userInfo: nil)
+        }
       }
-    }
-    catch {
-      body = request.httpBody
-        .map { ["\n\(String(decoding: $0, as: UTF8.self))"] }
-        ?? []
-    }
+      catch {
+        body = request.httpBody
+          .map { ["\n\(String(decoding: $0, as: UTF8.self))"] }
+          ?? []
+      }
 
-    return ([method] + headers + body).joined(separator: "\n")
+      return ([method] + headers + body).joined(separator: "\n")
+    }
   }
 }
