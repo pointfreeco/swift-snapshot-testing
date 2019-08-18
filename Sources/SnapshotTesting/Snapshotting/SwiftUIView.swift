89abdd95d1ba5271e352527073a82caef1b44440
Nate <nathan.mann@me.com>
2019-08-18 17:33:40 -0400

    added comments

--- Sources/SnapshotTesting/Snapshotting/SwiftUIView.swift
+++ Sources/SnapshotTesting/Snapshotting/SwiftUIView.swift
@@ -4,14 +4,21 @@ import Foundation
 import SwiftUI
 #endif
 
-
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+@available(iOS 13.0,  tvOS 13.0, *)
 extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
-     
+   
+  /// A snapshot strategy for comparing SwiftUI Views based on pixel equality.
   public static var image: Snapshotting {
     return .image()
   }
-        
+    
+  /// A snapshot strategy for comparing SwiftUI Views based on pixel equality.
+  ///
+  /// - Parameters:
+  ///   - config: A set of device configuration settings.
+  ///   - precision: The percentage of pixels that must match.
+  ///   - size: A view size override.
+  ///   - traits: A trait collection override.
   public static func image(
     on config: ViewImageConfig,
     precision: Float = 1,
@@ -27,13 +34,20 @@ extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
         .pullback(UIHostingController.init(rootView:))
     }
     
+    /// A snapshot strategy for comparing SwiftUI Views based on pixel equality.
+    ///
+    /// - Parameters:
+    ///   - drawHierarchyInKeyWindow: Utilize the simulator's key window in order to render `UIAppearance` and `UIVisualEffect`s. This option requires a host application for your tests and will _not_ work for framework test targets.
+    ///   - precision: The percentage of pixels that must match.
+    ///   - size: A view size override.
+    ///   - traits: A trait collection override.
     public static func image(
       drawHierarchyInKeyWindow: Bool = false,
       precision: Float = 1,
       size: CGSize? = nil,
       traits: UITraitCollection = .init()
-    )
-    -> Snapshotting {
+     )
+     -> Snapshotting {
       return Snapshotting<UIViewController, UIImage>
         .image(drawHierarchyInKeyWindow: drawHierarchyInKeyWindow,
                precision: precision,
@@ -42,3 +56,5 @@ extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
         .pullback(UIHostingController.init(rootView:))
     }
 }
+
+
