7dd2d1358afbfa85c6cd2802b814e16d363521d7
Jeffrey Macko <mackoj@users.noreply.github.com>
2020-05-07 13:48:43 +0200

    Update UIImage.swift
    Update image size for the new texte size.

--- Sources/SnapshotTesting/Snapshotting/UIImage.swift
+++ Sources/SnapshotTesting/Snapshotting/UIImage.swift
@@ -36,10 +36,11 @@ extension Diffing where Value == UIImage {
   
   /// Used when the image size has no width or no height to generated the default empty image
   private static func emptyImage() -> UIImage {
-    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 80))
     label.backgroundColor = .red
     label.text = "Error: No image could be generated for this view as its size was zero. Please set an explicit size in the test."
     label.textAlignment = .center
+    label.numberOfLines = 3
     return label.asImage()
   }
 }
