e04b8e13b6275c5cb779218d52860fb781a6d010
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-07-02 15:51:37 +0200

    Remove view is UIWindow || in condition Rely on configuration options instead.

--- Sources/SnapshotTesting/Common/View.swift
+++ Sources/SnapshotTesting/Common/View.swift
@@ -749,7 +749,7 @@ func snapshotView(
       addImagesForRenderedViews(view).sequence().run { views in
         callback(
           renderer(bounds: view.bounds, for: traits).image { ctx in
-            if view is UIWindow || drawHierarchyInKeyWindow {
+            if drawHierarchyInKeyWindow {
               view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
             } else {
               view.layer.render(in: ctx.cgContext)
