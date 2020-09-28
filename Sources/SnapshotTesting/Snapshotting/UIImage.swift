da4c83f1281ab2315b8fbf6fa889601179a51863
Vincent Esche <regexident@gmail.com>
2020-09-28 23:30:52 +0200

    Implemented `Diffing<UIImage>` based on `Diffing<CGImage>`

--- Sources/SnapshotTesting/Snapshotting/UIImage.swift
+++ Sources/SnapshotTesting/Snapshotting/UIImage.swift
@@ -12,36 +12,26 @@ extension Diffing where Value == UIImage {
   /// - Returns: A new diffing strategy.
   public static func image(precision: Float) -> Diffing {
     return Diffing(
-      toData: { $0.pngData() ?? emptyImage().pngData()! },
-      fromData: { UIImage(data: $0, scale: UIScreen.main.scale)! }
-    ) { old, new in
-      guard !compare(old, new, precision: precision) else { return nil }
-      let difference = SnapshotTesting.diff(old, new)
-      let message = new.size == old.size
-        ? "Newly-taken snapshot does not match reference."
-        : "Newly-taken snapshot@\(new.size) does not match reference@\(old.size)."
-      let oldAttachment = XCTAttachment(image: old)
-      oldAttachment.name = "reference"
-      let newAttachment = XCTAttachment(image: new)
-      newAttachment.name = "failure"
-      let differenceAttachment = XCTAttachment(image: difference)
-      differenceAttachment.name = "difference"
-      return (
-        message,
-        [oldAttachment, newAttachment, differenceAttachment]
-      )
-    }
+      toData: { Self.toData($0)! },
+      fromData: { Self.fromData($0)! },
+      diff: { Self.diff($0, $1, precision: precision) }
+    )
   }
 
+  public static func toData(_ image: Value) -> Data? {
+    image.pngData()
+  }
 
-  /// Used when the image size has no width or no height to generated the default empty image
-  private static func emptyImage() -> UIImage {
-    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 80))
-    label.backgroundColor = .red
-    label.text = "Error: No image could be generated for this view as its size was zero. Please set an explicit size in the test."
-    label.textAlignment = .center
-    label.numberOfLines = 3
-    return label.asImage()
+  public static func fromData(_ data: Data) -> Value? {
+    UIImage(data: data, scale: UIScreen.main.scale)
+  }
+
+  public static func diff(_ old: Value, _ new: Value, precision: Float) -> (String, [XCTAttachment])? {
+    return Diffing<CGImage>.diff(
+      old.cgImage!,
+      new.cgImage!,
+      precision: precision
+    )
   }
 }
 
@@ -55,77 +45,10 @@ extension Snapshotting where Value == UIImage, Format == UIImage {
   ///
   /// - Parameter precision: The percentage of pixels that must match.
   public static func image(precision: Float) -> Snapshotting {
-    return .init(
+    return Snapshotting(
       pathExtension: "png",
       diffing: .image(precision: precision)
     )
   }
-}
-
-private func compare(_ old: UIImage, _ new: UIImage, precision: Float) -> Bool {
-  guard let oldCgImage = old.cgImage else { return false }
-  guard let newCgImage = new.cgImage else { return false }
-  guard oldCgImage.width != 0 else { return false }
-  guard newCgImage.width != 0 else { return false }
-  guard oldCgImage.width == newCgImage.width else { return false }
-  guard oldCgImage.height != 0 else { return false }
-  guard newCgImage.height != 0 else { return false }
-  guard oldCgImage.height == newCgImage.height else { return false }
-  // Values between images may differ due to padding to multiple of 64 bytes per row,
-  // because of that a freshly taken view snapshot may differ from one stored as PNG.
-  // At this point we're sure that size of both images is the same, so we can go with minimal `bytesPerRow` value
-  // and use it to create contexts.
-  let minBytesPerRow = min(oldCgImage.bytesPerRow, newCgImage.bytesPerRow)
-  let byteCount = minBytesPerRow * oldCgImage.height
-
-  var oldBytes = [UInt8](repeating: 0, count: byteCount)
-  guard let oldContext = context(for: oldCgImage, bytesPerRow: minBytesPerRow, data: &oldBytes) else { return false }
-  guard let oldData = oldContext.data else { return false }
-  if let newContext = context(for: newCgImage, bytesPerRow: minBytesPerRow), let newData = newContext.data {
-    if memcmp(oldData, newData, byteCount) == 0 { return true }
-  }
-  let newer = UIImage(data: new.pngData()!)!
-  guard let newerCgImage = newer.cgImage else { return false }
-  var newerBytes = [UInt8](repeating: 0, count: byteCount)
-  guard let newerContext = context(for: newerCgImage, bytesPerRow: minBytesPerRow, data: &newerBytes) else { return false }
-  guard let newerData = newerContext.data else { return false }
-  if memcmp(oldData, newerData, byteCount) == 0 { return true }
-  if precision >= 1 { return false }
-  var differentPixelCount = 0
-  let threshold = 1 - precision
-  for byte in 0..<byteCount {
-    if oldBytes[byte] != newerBytes[byte] { differentPixelCount += 1 }
-    if Float(differentPixelCount) / Float(byteCount) > threshold { return false}
-  }
-  return true
-}
-
-private func context(for cgImage: CGImage, bytesPerRow: Int, data: UnsafeMutableRawPointer? = nil) -> CGContext? {
-  guard
-    let space = cgImage.colorSpace,
-    let context = CGContext(
-      data: data,
-      width: cgImage.width,
-      height: cgImage.height,
-      bitsPerComponent: cgImage.bitsPerComponent,
-      bytesPerRow: bytesPerRow,
-      space: space,
-      bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
-    )
-    else { return nil }
-
-  context.draw(cgImage, in: CGRect(x: 0, y: 0, width: cgImage.width, height: cgImage.height))
-  return context
-}
-
-private func diff(_ old: UIImage, _ new: UIImage) -> UIImage {
-  let width = max(old.size.width, new.size.width)
-  let height = max(old.size.height, new.size.height)
-  UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), true, 0)
-  new.draw(at: .zero)
-  old.draw(at: .zero, blendMode: .difference, alpha: 1)
-  let differenceImage = UIGraphicsGetImageFromCurrentImageContext()!
-  UIGraphicsEndImageContext()
-  return differenceImage
 }
 #endif
