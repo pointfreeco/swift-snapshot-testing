ca1bb345ce299bba50f00d7e6c0decfcdb5c4a38
Jeffrey Macko <github.jm@macko.fr>
2020-11-02 13:00:45 +0100

    [feat] Add support for XCUIElement

---
+++ Sources/SnapshotTesting/Snapshotting/XCUIElement.swift
@@ -0,0 +1,15 @@
+import XCTest
+import SnapshotTesting
+import UIKit
+
+extension Snapshotting where Value: XCUIElement, Format == UIImage {
+    public static var image: Snapshotting {
+    return Snapshotting<UIImage, UIImage>.image.asyncPullback { element in
+      Async<UIImage> { callback in
+        DispatchQueue.main.async {
+          callback(element.screenshot().image)
+        }
+      }
+    }
+  }
+}
