428902c87b6e7a416763aa38d92d8200af52bb43
Stephen Celis <stephen@stephencelis.com>
2022-09-30 17:22:25 -0700

    Clean up: don't ignore Package.resolved, exclude test fixtures

--- Package.swift
+++ Package.swift
@@ -18,7 +18,11 @@ let package = Package(
     .target(name: "SnapshotTesting"),
     .testTarget(
       name: "SnapshotTestingTests",
-      dependencies: ["SnapshotTesting"]
+      dependencies: ["SnapshotTesting"],
+      exclude: [
+        "__Fixtures__",
+        "__Snapshots__"
+      ]
     )
   ]
 )
