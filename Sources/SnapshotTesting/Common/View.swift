077de2808af6bf7f97c4ce392ab1dae19f604758
Niemiec, Michal [CWM] <michal.niemiec@ln.email.gs.com>
2022-10-21 12:58:48 +0200

    Add ViewImageConfig for iPhone 14, iPhone 14 Plus, iPhone 14 Pro and iPhone 14 Pro Max

--- Sources/SnapshotTesting/Common/View.swift
+++ Sources/SnapshotTesting/Common/View.swift
@@ -241,6 +241,52 @@ public struct ViewImageConfig {
     return .init(safeArea: safeArea, size: size, traits: .iPhone13ProMax(orientation))
   }
 
+  public static let iPhone14 = ViewImageConfig.iPhone14(.portrait)
+
+  public static func iPhone14(_ orientation: Orientation) -> ViewImageConfig {
+      .iPhone13(orientation)
+  }
+
+  public static let iPhone14Plus = ViewImageConfig.iPhone14Plus(.portrait)
+
+  public static func iPhone14Plus(_ orientation: Orientation) -> ViewImageConfig {
+      .iPhone13ProMax(orientation)
+  }
+
+   public static let iPhone14Pro = ViewImageConfig.iPhone14Pro(.portrait)
+
+   public static func iPhone14Pro(_ orientation: Orientation) -> ViewImageConfig {
+       let safeArea: UIEdgeInsets
+       let size: CGSize
+       switch orientation {
+       case .landscape:
+           safeArea = .init(top: 0, left: 59, bottom: 21, right: 59)
+           size = .init(width: 852, height: 393)
+       case .portrait:
+           safeArea = .init(top: 59, left: 0, bottom: 34, right: 0)
+           size = .init(width: 393, height: 852)
+       }
+
+       return .init(safeArea: safeArea, size: size, traits: .iPhone13ProMax(orientation))
+   }
+
+   public static let iPhone14ProMax = ViewImageConfig.iPhone14ProMax(.portrait)
+
+   public static func iPhone14ProMax(_ orientation: Orientation) -> ViewImageConfig {
+       let safeArea: UIEdgeInsets
+       let size: CGSize
+       switch orientation {
+       case .landscape:
+           safeArea = .init(top: 0, left: 59, bottom: 21, right: 59)
+           size = .init(width: 932, height: 430)
+       case .portrait:
+           safeArea = .init(top: 59, left: 0, bottom: 34, right: 0)
+           size = .init(width: 430, height: 932)
+       }
+
+       return .init(safeArea: safeArea, size: size, traits: .iPhone13ProMax(orientation))
+  }
+
   public static let iPadMini = ViewImageConfig.iPadMini(.landscape)
 
   public static func iPadMini(_ orientation: Orientation) -> ViewImageConfig {
