438abf3345828d83b3a82ae1af5a511b89181b39
Justas Liola <Justaslj@gmail.com>
2023-06-19 07:56:31 +0300

    Added snapshots with default and wait

--- Tests/SnapshotTestingTests/DelayTests.swift
+++ Tests/SnapshotTestingTests/DelayTests.swift
@@ -5,49 +5,59 @@ class DelayTests: XCTestCase {
   func testDelayViewDidLoad() {
     let  sut = DelayedViewController()
     
-      assertSnapshot(matching: sut, as: .image(on: .iPhone13, delay: 2))
+    assertSnapshot(matching: sut, as: .image(on: .iPhone13, delay: 2))
+  }
+  func testViewDidLoad() {
+    let  sut = DelayedViewController()
+    
+    assertSnapshot(matching: sut, as: .image(on: .iPhone13))
+  }
+  func testWaitViewDidLoad() {
+      let  sut = DelayedViewController()
+      
+    assertSnapshot(matching: sut, as: .wait(for: 2, on: .image(on: .iPhone13)))
   }
 }
 
-final class DelayedViewController: UIViewController {
+private final class DelayedViewController: UIViewController {
+  
+  private lazy var topLabel = UILabel()
+  private lazy var bottomLabel = UILabel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    private lazy var topLabel = UILabel()
-    private lazy var bottomLabel = UILabel()
+    setupUI()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupUI()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.topLabel.text = "Goodbye viewDidLoad"
-        }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      self.topLabel.text = "Goodbye viewDidLoad"
     }
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.bottomLabel.text = "Goodbye viewDidAppear"
-        }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      self.bottomLabel.text = "Goodbye viewDidAppear"
     }
+  }
+  
+  private func setupUI() {
+    view.backgroundColor = .white
     
-    private func setupUI() {
-        view.backgroundColor = .white
-        
-        topLabel.text = "Hello viewDidLoad"
-        bottomLabel.text = "Hello viewDidAppear"
-        topLabel.translatesAutoresizingMaskIntoConstraints = false
-        bottomLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(topLabel)
-        view.addSubview(bottomLabel)
-
-        NSLayoutConstraint.activate([
-            topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            topLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            bottomLabel.topAnchor.constraint(equalTo: topLabel.bottomAnchor),
-            bottomLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-    }
+    topLabel.text = "Hello viewDidLoad"
+    bottomLabel.text = "Hello viewDidAppear"
+    topLabel.translatesAutoresizingMaskIntoConstraints = false
+    bottomLabel.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(topLabel)
+    view.addSubview(bottomLabel)
+    
+    NSLayoutConstraint.activate([
+      topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      topLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      bottomLabel.topAnchor.constraint(equalTo: topLabel.bottomAnchor),
+      bottomLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    ])
+  }
 }
