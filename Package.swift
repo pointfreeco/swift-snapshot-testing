f7a96ae87d388295a7f83afbe305ca906e581c30
Stephen Celis <stephen@stephencelis.com>
2024-06-12 09:31:28 -0700

    Support swift-syntax from 600.0.0-latest
    The Xcode 16 beta generates macro projects using these swift-syntax
    snapshots. Luckily things seem to be backwards compatible, so we can
    expand our supported range.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", "508.0.1"..<"511.0.0")
+    .package(url: "https://github.com/apple/swift-syntax", "508.0.1"..<"601.0.0")
   ],
   targets: [
     .target(
