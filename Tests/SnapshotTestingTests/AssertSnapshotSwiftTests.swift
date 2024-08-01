8105068ea4d120523b066822905cbabc20d7f1b9
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2024-08-01 18:13:59 -0400

    Don't invoke XCTContext when running in the Testing framework. (#884)

---
+++ Tests/SnapshotTestingTests/AssertSnapshotSwiftTests.swift
@@ -0,0 +1,19 @@
+#if canImport(Testing)
+  import Testing
+  import Foundation
+  import InlineSnapshotTesting
+  @_spi(Experimental) import SnapshotTesting
+
+  @Suite(
+    .snapshots(
+      record: .missing
+    )
+  )
+  struct AssertSnapshotTests {
+    @Test func dump() {
+      struct User { let id: Int, name: String, bio: String }
+      let user = User(id: 1, name: "Blobby", bio: "Blobbed around the world.")
+      assertSnapshot(of: user, as: .dump)
+    }
+  }
+#endif
