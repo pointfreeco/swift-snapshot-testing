bddcea0f8c8da89a03e17653bea864a5e4684b36
Jeffrey Macko <github.jm@macko.fr>
2024-09-11 12:46:47 +0200

    fix: improve documentation

--- Sources/SnapshotTesting/Plug-ins/PluginRegistry.swift
+++ Sources/SnapshotTesting/Plug-ins/PluginRegistry.swift
@@ -29,20 +29,39 @@ public class PluginRegistry {
     return Array(plugins.values).compactMap { $0 as? ImageSerialization  }
   }
     
+  /// Registers all classes that conform to the `SnapshotTestingPlugin` protocol.
+  ///
+  /// This function iterates over all classes known to the Objective-C runtime and registers any class
+  /// that conforms to the `SnapshotTestingPlugin` protocol as a plugin. The plugin classes are expected to
+  /// implement the `SnapshotTestingPlugin` protocol and have a parameterless initializer.
+  ///
+  /// The process is as follows:
+  /// 1. The function first queries the Objective-C runtime to get the total number of classes.
+  /// 2. It allocates memory to hold references to these classes.
+  /// 3. It retrieves all class references into the allocated memory.
+  /// 4. It then iterates through each class reference, checking if it conforms to the `SnapshotTestingPlugin` protocol.
+  /// 5. If a class conforms, it is instantiated and registered as a plugin using the `registerPlugin(_:)` method.
   func registerAllPlugins() {
-    let count = objc_getClassList(nil, 0)
-    let classes = UnsafeMutablePointer<AnyClass?>.allocate(capacity: Int(count))
-    let autoreleasingClasses = AutoreleasingUnsafeMutablePointer<AnyClass>(classes)
-    objc_getClassList(autoreleasingClasses, count)
+    let classCount = objc_getClassList(nil, 0)
+    guard classCount > 0 else { return }
     
-    for i in 0..<Int(count) {
-      if let cls: AnyClass = classes[i], class_conformsToProtocol(cls, SnapshotTestingPlugin.self) {
-        if let pluginType = cls as? SnapshotTestingPlugin.Type {
-          self.registerPlugin(pluginType.init())
-        }
+    let classes = UnsafeMutablePointer<AnyClass?>.allocate(capacity: Int(classCount))
+    defer { classes.deallocate() }
+    
+    let autoreleasingClasses = AutoreleasingUnsafeMutablePointer<AnyClass>(classes)
+    objc_getClassList(autoreleasingClasses, classCount)
+    
+    for i in 0..<Int(classCount) {
+      guard
+        let someClass = classes[i],
+        class_conformsToProtocol(someClass, SnapshotTestingPlugin.self),
+        let pluginType = someClass as? SnapshotTestingPlugin.Type
+      else {
+        continue
       }
+      self.registerPlugin(pluginType.init())
     }
-    classes.deallocate()
   }
+
 }
 #endif
