c282c85a4aa6eaa28c2227d4cdf59b8651b21a68
Oguz Yuksel <oguz.yueksel@teufel.de>
2025-01-12 02:26:45 +0100

    handle circular references during reccursive child search

--- Sources/SnapshotTesting/Snapshotting/Any.swift
+++ Sources/SnapshotTesting/Snapshotting/Any.swift
@@ -66,12 +66,26 @@ extension Snapshotting where Format == String {
   }
 }
 
-private func snap<T>(_ value: T, name: String? = nil, indent: Int = 0) -> String {
+private func snap<T>(
+  _ value: T,
+  name: String? = nil,
+  indent: Int = 0,
+  visitedValues: Set<ObjectIdentifier> = .init()
+) -> String {
   let indentation = String(repeating: " ", count: indent)
   let mirror = Mirror(reflecting: value)
   var children = mirror.children
   let count = children.count
   let bullet = count == 0 ? "-" : "▿"
+  var visitedValues = visitedValues
+
+  if let valueAsObject = value as? AnyObject {
+    let objectID = ObjectIdentifier(valueAsObject)
+    if visitedValues.contains(objectID) {
+      return "\(indentation)\(bullet) \(name ?? "value") (circular reference detected)\n"
+    }
+    visitedValues.insert(objectID)
+  }
 
   let description: String
   switch (value, mirror.displayStyle) {
@@ -79,10 +93,10 @@ private func snap<T>(_ value: T, name: String? = nil, indent: Int = 0) -> String
     description = count == 1 ? "1 element" : "\(count) elements"
   case (_, .dictionary?):
     description = count == 1 ? "1 key/value pair" : "\(count) key/value pairs"
-    children = sort(children)
+    children = sort(children, visitedValues: visitedValues)
   case (_, .set?):
     description = count == 1 ? "1 member" : "\(count) members"
-    children = sort(children)
+    children = sort(children, visitedValues: visitedValues)
   case (_, .tuple?):
     description = count == 1 ? "(1 element)" : "(\(count) elements)"
   case (_, .optional?):
@@ -98,7 +112,7 @@ private func snap<T>(_ value: T, name: String? = nil, indent: Int = 0) -> String
   case (_, .class?), (_, .struct?):
     description = String(describing: mirror.subjectType)
       .replacingOccurrences(of: " #\\d+", with: "", options: .regularExpression)
-    children = sort(children)
+    children = sort(children, visitedValues: visitedValues)
   case (_, .enum?):
     let subjectType = String(describing: mirror.subjectType)
       .replacingOccurrences(of: " #\\d+", with: "", options: .regularExpression)
@@ -109,15 +123,15 @@ private func snap<T>(_ value: T, name: String? = nil, indent: Int = 0) -> String
 
   let lines =
     ["\(indentation)\(bullet) \(name.map { "\($0): " } ?? "")\(description)\n"]
-    + children.map { snap($1, name: $0, indent: indent + 2) }
+    + children.map { snap($1, name: $0, indent: indent + 2, visitedValues: visitedValues) }
 
   return lines.joined()
 }
 
-private func sort(_ children: Mirror.Children) -> Mirror.Children {
+private func sort(_ children: Mirror.Children, visitedValues: Set<ObjectIdentifier>) -> Mirror.Children {
   return .init(
     children
-      .map({ (child: $0, snap: snap($0)) })
+      .map({ (child: $0, snap: snap($0, visitedValues: visitedValues)) })
       .sorted(by: { $0.snap < $1.snap })
       .map({ $0.child })
   )
