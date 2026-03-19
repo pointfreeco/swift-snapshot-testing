9a7a1c797a049db367ba90dda5252f61b26fc8ce
Brandon Williams <mbrandonw@hey.com>
2026-03-19 09:36:11 -0500

    clean up

--- Tests/SnapshotTestingTests/SwiftTestingTests.swift
+++ Tests/SnapshotTestingTests/SwiftTestingTests.swift
@@ -30,39 +30,25 @@
         }
       }
 
-      #if canImport(UIKit)
       @Test(
         .enabled {
           !ProcessInfo.processInfo.environment.keys.contains("GITHUB_WORKFLOW")
         }
       )
       func testUIImage() {
-        let redPixel = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { context in
-          UIColor.red.setFill()
-          context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-        }
-        let bluePixel = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { context in
-          UIColor.blue.setFill()
-          context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-        }
-        assertSnapshot(of: redPixel, as: .image, named: "pixel")
-        withKnownIssue {
-          assertSnapshot(of: bluePixel, as: .image, named: "pixel")
-        } matching: { issue in
-          issue.description.hasSuffix(
-            "Newly-taken snapshot does not match reference."
-          )
-        }
-      }
-      #endif
-
-      #if canImport(AppKit)
-        @Test(
-          .enabled {
-            !ProcessInfo.processInfo.environment.keys.contains("GITHUB_WORKFLOW")
+        #if canImport(UIKit)
+          let redPixel = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image {
+            context in
+            UIColor.red.setFill()
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
           }
-        )
-        func testImage() {
+          let bluePixel = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image {
+            context in
+            UIColor.blue.setFill()
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+          }
+        #endif
+        #if canImport(AppKit)
           let redPixel = NSImage(size: NSSize(width: 1, height: 1), flipped: false) { rect in
             NSColor.red.setFill()
             rect.fill()
@@ -73,16 +59,16 @@
             rect.fill()
             return true
           }
-          assertSnapshot(of: redPixel, as: .image, named: "pixel")
-          withKnownIssue {
-            assertSnapshot(of: bluePixel, as: .image, named: "pixel")
-          } matching: { issue in
-            issue.description.hasSuffix(
-              "Newly-taken snapshot does not match reference."
-            )
-          }
+        #endif
+        assertSnapshot(of: redPixel, as: .image, named: "pixel")
+        withKnownIssue {
+          assertSnapshot(of: bluePixel, as: .image, named: "pixel")
+        } matching: { issue in
+          issue.description.hasSuffix(
+            "Newly-taken snapshot does not match reference."
+          )
         }
-      #endif
+      }
     }
   }
 #endif
