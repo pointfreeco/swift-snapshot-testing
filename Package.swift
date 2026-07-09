b736388c041fdd43c8a79cc70d003c170d686b9b
Brandon Williams <mbrandonw@hey.com>
2026-07-09 15:25:21 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
   ],
   dependencies: [
     .package(
-      url: "https://github.com/apple/swift-syntax.git",
+      url: "https://github.com/swiftlang/swift-syntax",
       from: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-09-05-a"
     )
   ],
