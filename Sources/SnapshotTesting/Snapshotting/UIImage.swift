31328b5bf43913c29e66a0f2f82ef7ea86d34a85
Adam Alix <adam@flashback.nyc>
2026-09-16 09:33:40 -0400

    Fix perceptual image comparison on macOS 27

--- Sources/SnapshotTesting/Snapshotting/UIImage.swift
+++ Sources/SnapshotTesting/Snapshotting/UIImage.swift
@@ -384,11 +384,11 @@ private func normalizedComponentDiff(_ old: UIImage, _ new: UIImage) -> UIImage?
     }
 
     func applyingAreaAverage() -> CIImage {
-      applyingFilter("CIAreaAverage", parameters: [kCIInputExtentKey: extent])
+      applyingFilter("CIAreaAverage", parameters: [kCIInputExtentKey: CIVector(cgRect: extent)])
     }
 
     func applyingAreaMaximum() -> CIImage {
-      applyingFilter("CIAreaMaximum", parameters: [kCIInputExtentKey: extent])
+      applyingFilter("CIAreaMaximum", parameters: [kCIInputExtentKey: CIVector(cgRect: extent)])
     }
 
     func renderSingleValue(in context: CIContext) -> Float? {
